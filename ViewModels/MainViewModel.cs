72b867cfacc7ddcac5c368f4448af415b882562b
bulletsman04 <bulletsman04@gmail.com>
2018-12-04 22:07:33 +0100

    Basic hierarchy of project with topbar

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class MainViewModel
+    {
+        public MenuViewModel MenuViewModel { get; set; }
+        public MainViewModel()
+        {
+            MenuViewModel = new MenuViewModel();
+        }
+    }
+}
